caae7bf43f57ca69f340f59720d86f8248f072c4
arosen64 <arosen64@jh.edu>
2025-07-31 12:37:30 -0400

    now pass basicmarketbid test

--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -6,14 +6,14 @@ FillReport OrderBook::handleLimitOrder(Order& order) {
         return FillReport(OrderResult::REJECTED, 0, std::vector<std::pair<Price, int> >());
     }
 
-    if (order.side == Side::BUY && asks.empty()) {
+    if ((order.side == Side::BUY && asks.empty()) || (order.price < asks.begin()->first)) {
         bids[order.price].push_back(order);
         OrderDetails details = {order.side, order.price, --bids[order.price].end()};
         orderMap[order.orderId] = details;
         return FillReport(OrderResult::RESTED, 0, std::vector<std::pair<Price, int> >());
     }
 
-    if (order.side == Side::SELL && bids.empty()) {
+    if ((order.side == Side::SELL && bids.empty()) || (order.price > bids.rbegin()->first)) {
         asks[order.price].push_back(order);
         OrderDetails details = {order.side, order.price, --asks[order.price].end()};
         orderMap[order.orderId] = details;
@@ -27,7 +27,40 @@ FillReport OrderBook::handleLimitOrder(Order& order) {
 FillReport OrderBook::handleMarketOrder(Order& order) {
     if (order.side == Side::BUY && asks.empty()) {
         return FillReport(OrderResult::REJECTED, 0, std::vector<std::pair<Price, int> >());
+    } else {
+        FillReport deets = FillReport(OrderResult::REJECTED, 0, std::vector<std::pair<Price, int> >());
+        auto it = asks.begin();
+
+        // iterating through the prices in the book to fill the market bid
+        while (it != asks.end() && order.quantity > 0) {
+            // iterating through all the offers at the best price to fill the market bid
+            while (!it->second.empty()) {
+                int fillQuantity = std::min(order.quantity, it->second.front().quantity);
+                order.quantity -= fillQuantity;
+                deets.filledQuantity += fillQuantity;
+                
+                // decreasing the limit order quantity at this price
+                it->second.front().quantity -= fillQuantity;
+                deets.transactionPrices.push_back(std::pair<Price, int>(it->first, fillQuantity));
+                int lastOrderId = it->second.front().orderId;
+
+
+                if (it->second.front().quantity == 0) {
+                    // if the order is fully filled, remove it from the book
+                    it->second.pop_front();
+                    orderMap.erase(lastOrderId);
+                }
+            }
+        }
+        // if the order is fully filled then indicate that is the case
+        if (order.quantity == 0) {
+            deets.status = OrderResult::FILLED;
+        } else if (deets.filledQuantity > 0) {
+            deets.status = OrderResult::PARTIALLY_FILLED;
+        }
+        return deets;
     }
+
     
     if (order.side == Side::SELL && bids.empty()) {
         return FillReport(OrderResult::REJECTED, 0, std::vector<std::pair<Price, int> >());
